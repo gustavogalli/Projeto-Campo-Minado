d67611689b8a43c06c191c71ff7c27e9d12247c3
Gustavo Galli <gustavosgalli@gmail.com>
2021-08-09 16:32:46 -0300

    Adicionando classe TabuleiroConsole com método cicloDoJogo e SairException

--- src/br/com/cod3r/cm/Aplicacao.java
+++ src/br/com/cod3r/cm/Aplicacao.java
@@ -1,14 +1,14 @@
 package br.com.cod3r.cm;
 
 import br.com.cod3r.cm.modelo.Tabuleiro;
+import br.com.cod3r.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 	public static void main(String[] args) {
-		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
 		
-		tabuleiro.abrirCampo(3, 3);
-		tabuleiro.marcarCampo(4, 4);
-		System.out.println(tabuleiro);
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+		
 		
 	}
 }
